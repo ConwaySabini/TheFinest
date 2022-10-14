ff1ee5477adc23dfdb6ccbd22bd4ce29e72230db
Ethan Sabini <ConwaySabini@Outlook.com>
2022-10-14 14:26:55 -0700

    husky

---
+++ apps/client/components/Home.jsx
@@ -0,0 +1,11 @@
+import styles from '../styles/Home.module.css';
+import SideBar from './SideBar';
+const Home = () => {
+    return (<div className={styles.parent}>
+      <div className={styles.leftCol}>
+        <SideBar></SideBar>
+      </div>
+    </div>);
+};
+export default Home;
+//# sourceMappingURL=Home.jsx.map
